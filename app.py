ccb0fc61d58fecb2749029e0fccc090032d69eb7
molinaromaissae1 <molinaromaissae1@gmail.com>
2026-03-12 01:02:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,41 @@
 import streamlit as st
 from reader import read_cv
-from features import extract_experience, extract_education, extract_skills, extract_languages
+
+from features import (
+    extract_experience,
+    extract_education,
+    extract_skills,
+    extract_languages,
+    extract_sector,
+    extract_companies
+)
 
 st.title("AI CV Screening Agent")
 
-uploaded_file = st.file_uploader("Upload CV", type=["pdf", "docx"])
+uploaded_file = st.file_uploader("Upload CV", type=["pdf","docx"])
 
 if uploaded_file is not None:
 
     text = read_cv(uploaded_file)
+
     st.subheader("Text detected in CV")
     st.write(text)
+
     st.subheader("Extracted Information")
 
     experience = extract_experience(text)
     education = extract_education(text)
     skills = extract_skills(text)
     languages = extract_languages(text)
+    sector = extract_sector(text)
+    companies = extract_companies(text)
 
-    st.write("Experience:", experience)
-    st.write("Education:", education)
+    st.write("Years of experience:", experience)
+    st.write("Education level:", education)
     st.write("Skills:", skills)
     st.write("Languages:", languages)
+    st.write("Sector:", sector)
+    st.write("Number of companies:", companies)
 
     score = experience*2 + len(skills)*3 + len(languages)
 
