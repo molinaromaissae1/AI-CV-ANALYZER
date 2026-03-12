a0cccd215d8e13318b74fbb8987c2d606844fe2d
molinaromaissae1 <molinaromaissae1@gmail.com>
2026-03-12 02:07:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from reader import read_cv
+from reader import extract_text_from_pdf
+from preprocess import preprocess_text
 
 from features import (
     extract_experience,
@@ -10,39 +11,79 @@ from features import (
     extract_companies
 )
 
-st.title("AI CV Screening Agent")
+from ats_scoring import calculate_ats_score
 
-uploaded_file = st.file_uploader("Upload CV", type=["pdf","docx"])
+
+st.set_page_config(
+    page_title="AI CV Analyzer",
+    page_icon="🤖",
+    layout="wide"
+)
+
+st.title("🤖 AI CV Analyzer for HR")
+st.write("Upload a CV and the system will automatically analyze it.")
+
+
+uploaded_file = st.file_uploader(
+    "📄 Upload CV (PDF format)",
+    type=["pdf"]
+)
+
 
 if uploaded_file is not None:
 
-    text = read_cv(uploaded_file)
+    text = extract_text_from_pdf(uploaded_file)
 
-    st.subheader("Text detected in CV")
-    st.write(text)
+    clean_text = preprocess_text(text)
 
-    st.subheader("Extracted Information")
 
-    experience = extract_experience(text)
-    education = extract_education(text)
-    skills = extract_skills(text)
-    languages = extract_languages(text)
-    sector = extract_sector(text)
-    companies = extract_companies(text)
+    experience = extract_experience(clean_text)
+    education = extract_education(clean_text)
+    skills = extract_skills(clean_text)
+    languages = extract_languages(clean_text)
+    sector = extract_sector(clean_text)
+    companies = extract_companies(clean_text)
 
-    st.write("Years of experience:", experience)
-    st.write("Education level:", education)
-    st.write("Skills:", skills)
-    st.write("Languages:", languages)
-    st.write("Sector:", sector)
-    st.write("Number of companies:", companies)
+    score = calculate_ats_score(
+        experience,
+        skills,
+        languages,
+        education
+    )
 
-    score = experience*2 + len(skills)*3 + len(languages)
 
-    st.subheader("ATS Score")
-    st.write(score)
+    st.subheader("📊 Extracted Information")
 
-    if score > 10:
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.metric("Years of Experience", experience)
+        st.metric("Education Level", education)
+
+    with col2:
+        st.metric("Sector", sector)
+        st.metric("Companies", companies)
+
+
+    st.subheader("💼 Skills")
+
+    for skill in skills:
+        st.success(skill)
+
+
+    st.subheader("🌍 Languages")
+
+    for lang in languages:
+        st.info(lang)
+
+
+    st.subheader("📈 ATS Score")
+
+    st.progress(score)
+
+    st.write("Score:", score)
+
+    if score > 60:
         st.success("Candidate Selected")
     else:
-        st.error("Candidate Rejected")
+        st.warning("Candidate Not Selected")
